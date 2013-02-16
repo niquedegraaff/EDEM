4ec68856e6e0a4ac6c3913ce17972e4d60b341f9
Dominique de Graaff <niquedegraaff@gmail.com>
2013-02-16 15:19:22 +0100

    Added DB support (dev)

--- application/config/development/db.php
+++ application/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=edem_dev',
 			'username'   => 'root',
-			'password'   => 'root',
+			'password'   => '',
 		),
 	),
 );
